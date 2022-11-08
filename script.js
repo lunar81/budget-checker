aebcd46d9f57d5eb4f0cea7107b835c625b81020
Isaac Chavez <chavez.isaac93@gmail.com>
2022-11-07 17:23:08 -0800

    Added some logic to isolate the dayshours

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-let wages = document.querySelectorAll(".show-for-medium-up tr span[data-bind='currency: HOURLY_WAGE']")
 
-for (let i = 0; i < wages.length; i++) {
-    wages[i].innerHTML = wages[i].innerHTML.replace(/[+-]?[0-9]{1,3}(?:,?[0-9]{3})*\.[0-9]{2}$/, '90.00');
+
+attributeValues = ['currency: HOURLY_WAGE', 'text: SKILLS', 'text: CONDITIONS'];
+
+let wages = getData(attributeValues[0]);
+let skills = getData(attributeValues[1]);
+let conditions = getData(attributeValues[2]);
+
+dayAndTime = findDayTime(conditions[0].innerText)
+
+console.log("Day and Time: ", dayAndTime);
+
+console.log(wages);
+console.log(skills);
+console.log(conditions);
+
+const currencyRegex = /[+-]?[0-9]{1,3}(?:,?[0-9]{3})*\.[0-9]{2}$/;
+wages[0].innerHTML = wages[0].innerHTML.replace(currencyRegex, '90.00');
+
+function getData(dataAttribute) {
+    return document.querySelectorAll(`.show-for-medium-up tr span[data-bind='${dataAttribute}']`)
+}
+
+function findDayTime(conditionsString){
+    const dayAndTimeRegex = /(4|5|6)*\/(8|10|12)/;
+    return conditionsString.match(dayAndTimeRegex);
 }
